0cf6d05df21211d58191ce9713562f8f6df36696
akramjon997 <akramjonju1997@gmail.com>
2025-10-14 15:37:57 +0500

    Update vazifa1.sql

--- lesson-1/homework/vazifa1.sql
+++ lesson-1/homework/vazifa1.sql
@@ -3,6 +3,16 @@ database is electronic storage of organised or related data or collection of it
 relational database - data stored in tables  that are interconnected by common fields
 table is a collection of related date arranged in rows and collumns.
 
+  5 key feature of Sql Server
+  1. Database engine- The core service for storing, processing and securing data.
+  2. SQL Server management studio (SSMS) a powerful graphical tool for managing databases, writing queries, backing up/restoring, and monitoring performance.
+  3. Security  Features- includes authentication, authorization, encryption and row-level security to protect data from unauthorized access.
+  4. High availability and Disaster Recovery- Features like Always on availability groups, failover clustering, 
+  database mirroring ensure databases remain available  dur ing failures 
+  5. Business Intelligence (BI) and Analytics- Integreated tools such as Sql Server Reporting services (SSRS), 
+  integration Services (SSIS), and Analysis Services (SSAS) support data analysis, etl, and report generation.
+1. Authentication modes: Windows authentication mode  and SQL server authentication mode
+  
 Create database SchoolDB
   use SchoolDB
   Create table Students (StudentID INT PRIMARY KEY, Name VARCHAR(50), Age INT)
