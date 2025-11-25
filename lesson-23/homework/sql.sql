06e263c2a90c1ce7afea17b235ba1f2be2e5b14f
akramjon997 <akramjonju1997@gmail.com>
2025-11-25 18:46:45 +0500

    Update sql.sql

--- lesson-23/homework/sql.sql
+++ lesson-23/homework/sql.sql
@@ -33,3 +33,89 @@ group by Id
 			end as output 
 from TheZeroPuzzle.
 7.Select Sum(QuantitySold*UnitPrice) as TotalRevenue from Sales
+8.Select avg(unitPrice) AverageUnitPrice
+	from Sales
+9.Select count(SaleId) as Num_Transactions 
+	from Sales
+10.Select max(QuantitySold) as MaxQuantitySold
+from Sales
+11.Select Category, 
+		Sum(QuantitySold) as TotalUnitsSold 
+from Sales
+group by Category
+12.Select region, 
+			Sum(QuantitySold*UnitPrice) as TotalRevenue
+from Sales
+group by region
+13.Select top 1 Product,
+			Sum(QuantitySold*UnitPrice) as Revenue
+from Sales 
+group by Product
+order by Revenue desc
+14.Select SaleID,SaleDate,
+		QuantitySold*UnitPrice as Total,
+		Sum(QuantitySold*UnitPrice) over (order by SaleDate rows between unbounded preceding and current row) as RunningTotal
+from Sales
+15.Select Category,
+       Sum(QuantitySold*UnitPrice) as Total,
+	   Round(100* Sum(QuantitySold*UnitPrice)/(Select Sum(QuantitySold*UnitPrice) from Sales),2)
+	   as Percentage
+from Sales
+group by Category.
+17.Select s.*,c.CustomerName
+from Sales as s
+join Customers as c
+on s.CustomerID=c.CustomerID
+18.
+Select c.CustomerID,
+		c.CustomerName
+from Sales as s
+right join Customers as c
+on s.CustomerID=c.CustomerID
+where s.CustomerID is Null
+19.
+Select c.CustomerID,
+		c.CustomerName,
+		Sum(QuantitySold*UnitPrice) as Revenue
+from Sales as s
+right join Customers as c
+  on s.CustomerID=c.CustomerID
+group by c.CustomerID,
+		c.CustomerName
+20.
+Select top 1 c.CustomerID,
+		c.CustomerName,
+		Sum(QuantitySold*UnitPrice) as Revenue
+from Sales as s
+right join Customers as c
+  on s.CustomerID=c.CustomerID
+group by c.CustomerID,
+		c.CustomerName
+order by Revenue desc
+
+--21
+Select c.CustomerID,
+		c.CustomerName,
+		Sum(QuantitySold) as TotalSales
+from Sales as s
+right join Customers as c
+  on s.CustomerID=c.CustomerID
+group by c.CustomerID,
+		c.CustomerName
+--22
+	Select * from Products as p
+left join Sales as s
+on s.Product=p.ProductName
+where s.QuantitySold>0
+
+--23 
+Select  top 1 ProductID, ProductName, Category,SellingPrice from Products
+order by SellingPrice desc
+--24
+with cte as 
+(Select ProductID, ProductName, Category,SellingPrice, avg(sellingPrice) over (partition by Category) as Average_price
+from Products)
+Select ProductID, ProductName
+from cte
+where SellingPrice>Average_price
+
